d23c3b9b2d7dbbb5f3a13fb08ffaec527c33654e
gilberthappi <gdushimimana6@gmail.com>
2023-09-20 23:41:21 +0200

    Add comment Feature

---
+++ src/__mock__/countMovies.js
@@ -0,0 +1,11 @@
+const movies = (arr) => {
+    let length;
+    arr.forEach((element, i) => {
+      length = i + 1;
+    });
+    return length;
+  };
+  
+  // export default movies;
+  module.exports = movies;
+  
