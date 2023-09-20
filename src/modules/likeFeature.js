acd8e16fe7df25872273559260a9b814bb917da1
MarkYegon7 <markyegonk@gmail.com>
2023-09-20 20:14:50 +0300

    Add like feature file

--- src/modules/likeFeature.js
+++ src/modules/likeFeature.js
@@ -1,21 +1,21 @@
-// /* eslint-disable linebreak-style */
-// import fetchFromApi from './renderMovies.js';
-// import postLike from './likesApi.js';
+/* eslint-disable linebreak-style */
+import fetchFromApi from './renderMovies.js';
+import postLike from './likesApi.js';
 
-// const likeMovie = async () => {
-//   const allMovies = await fetchFromApi();
+const likeMovie = async () => {
+  const allMovies = await fetchFromApi();
 
-//   const likesBtn = document.querySelectorAll('.like');
-//   const spans = document.querySelectorAll('#count-like');
-//   likesBtn.forEach((a, i) => {
-//     let h = JSON.parse(spans[i].innerHTML);
+  const likesBtn = document.querySelectorAll('.like');
+  const spans = document.querySelectorAll('#count-like');
+  likesBtn.forEach((a, i) => {
+    let h = JSON.parse(spans[i].innerHTML);
 
-//     a.addEventListener('click', () => {
-//       postLike(allMovies[i].name);
-//       h += 1;
-//       spans[i].innerHTML = h;
-//     });
-//   });
-// };
+    a.addEventListener('click', () => {
+      postLike(allMovies[i].name);
+      h += 1;
+      spans[i].innerHTML = h;
+    });
+  });
+};
 
-// export default likeMovie;
+export default likeMovie;
